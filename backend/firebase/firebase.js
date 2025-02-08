6eb2d673830745b87b50688abe80129b9fff7cf8
Sonudhukia143 <jagdishdhukia770@gmail.com>
2025-02-08 22:08:38 +0530

    forth commit

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from firebase/app;
+import { initializeApp } from "firebase/app"; // Fixed the import statement
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   databaseURL: process.env.databaseURL,
   projectId: process.env.projectId,
-  storageBucket:process.env.storageBucket,
+  storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.messagingSenderId,
-  appId:process.env.appId,
+  appId: process.env.appId,
   measurementId: process.env.measurementId
 };
 
